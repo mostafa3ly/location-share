1e343c77cc22db95144a0f4d0bd6fa8a6988ca8f
Mostafa Aly <mostafa.aly25195@gmail.com>
2022-12-20 13:19:21 +0200

    add websocket server and test connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css';
+import { useEffect } from "react";
+import { w3cwebsocket } from "websocket";
+import "./App.css";
+
+const client = new w3cwebsocket("ws://127.0.0.1:8000");
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  useEffect(() => {
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+    client.onclose = () => {
+      console.log("closed");
+    };
+    return () => client.close();
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
